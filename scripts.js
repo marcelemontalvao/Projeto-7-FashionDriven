e01b890dd59ba16a79f325e82e3f0988169b4605
Marcele <marcele055@gmail.com>
2022-04-30 05:15:20 -0300

    Ajuste de bugs

--- scripts.js
+++ scripts.js
@@ -1,40 +1,33 @@
 // const nome = prompt("Qual o seu nome?")
- const modelo = document.querySelector(".modelo .selecionado")
- const gola = document.querySelector(".gola .selecionado") 
- const tecido = document.querySelector(".tecido .selecionado")
-
 function escolherModelo (elemento) {
-   
-    if (modelo === null) {
-        elemento.classList.toggle("selecionado")       
-    } else {
-        if (elemento.classList.contains("selecionado")) {
-        elemento.classList.remove("selecionado")
-        }
-    }
-
+const modelo = document.querySelector(".modelo .selecionado")
+   if (modelo === null) {
+       elemento.classList.toggle("selecionado")       
+   } else {
+       if (elemento.classList.contains("selecionado")) {
+       elemento.classList.remove("selecionado")
+       }
+   }
 }
 
 function escolherGola (elemento) {  
-  
-    if (gola === null) {
-        elemento.classList.toggle("selecionado")       
-    } else {
-        if (elemento.classList.contains("selecionado")) {
-        elemento.classList.remove("selecionado")
-        }
-    }
+const gola = document.querySelector(".gola .selecionado")
+   if (gola === null) {
+       elemento.classList.toggle("selecionado")       
+   } else {
+       if (elemento.classList.contains("selecionado")) {
+       elemento.classList.remove("selecionado")
+       }
+   }
 }
 
 function escolherTecido (elemento) {
-   
-    if (tecido === null) {
-        elemento.classList.toggle("selecionado")       
-    } else {
-        if (elemento.classList.contains("selecionado")) {
-        elemento.classList.remove("selecionado")
-        }
-    }  
+  const tecido = document.querySelector(".tecido .selecionado")
+   if (tecido === null) {
+       elemento.classList.toggle("selecionado")       
+   } else {
+       if (elemento.classList.contains("selecionado")) {
+       elemento.classList.remove("selecionado")
+       }
+   }  
 }
-
-
